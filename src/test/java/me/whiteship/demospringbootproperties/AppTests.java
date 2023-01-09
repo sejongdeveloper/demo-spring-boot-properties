0025bda77a2b8240ed2e05190ef220051a148d8e
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-09 21:50:47 +0900

    프로퍼티 변경

---
+++ src/test/java/me/whiteship/demospringbootproperties/AppTests.java
@@ -0,0 +1,13 @@
+package me.whiteship.demospringbootproperties;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
